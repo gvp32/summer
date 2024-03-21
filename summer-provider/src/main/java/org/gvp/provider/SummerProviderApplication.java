a02e490773470df129d3b27418e389067b60e0eb
wjj9132 <974442421@qq.com>
2024-03-21 20:53:16 +0800

    feat(provider): 添加服务提供者模块模块

---
+++ summer-provider/src/main/java/org/gvp/provider/SummerProviderApplication.java
@@ -0,0 +1,11 @@
+package org.gvp.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SummerProviderApplication.class, args);
+    }
+}
